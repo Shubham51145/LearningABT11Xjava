089003cd19a49082174a257225a6aa994c61ac06
nehakaremore <nehakaremore12@gmail.com>
2025-09-11 00:16:10 +0530

    10 sep 2025

--- src/ex_01_javabasic/HelloWorld.java
+++ src/ex_01_javabasic/HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_javabasic;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello shubham");
     }
 }
---
+++ src/ex_01_javabasic/Lab002_Helloshubhamprogram.java
@@ -0,0 +1,7 @@
+package ex_01_javabasic;
+
+public class Lab002_Helloshubhamprogram {
+    public static void main(String[] args) {
+        System.out.println("Hello karmore");
+    }
+}
